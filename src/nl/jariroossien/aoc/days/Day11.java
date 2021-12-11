0b62c973a1ffe27693168062a9ba53b1e21cbc73
Jari Roossien <jariroossien@gmail.com>
2021-12-11 06:32:56 +0100

    Cleaned up code, added comments.

--- src/nl/jariroossien/aoc/days/Day11.java
+++ src/nl/jariroossien/aoc/days/Day11.java
@@ -1,8 +1,5 @@
 package nl.jariroossien.aoc.days;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Day11 extends Day {
 
     int[][] grid;
@@ -11,29 +8,56 @@ public class Day11 extends Day {
     public long solveOne() {
         int flashes = 0;
         for (int i = 0; i < 100; i++) {
-            while (hasNines(grid)) {
-                for (int y = 0; y < grid.length; y++) {
-                    for (int x = 0; x < grid[0].length; x++) {
-                        if (grid[y][x] == 9) {
-                            explode(grid, y, x);
-                        }
-                    }
-                }
-            }
-            for (int y = 0; y < grid.length; y++) {
-                for (int x = 0; x < grid[0].length; x++) {
-                    if (grid[y][x] == 10) {
-                        grid[y][x] = 0;
+            explodeNines();
+            flashes += increaseValue();
+        }
+        return flashes;
+    }
+
+    /**
+     * After exploding all 9s, we increase the value of each item
+     * by 1. We replace all 10's with 0's to reset them.
+     *
+     * @return Amount of flashes that have happened.
+     */
+    private int increaseValue() {
+        int flashes = 0;
+        for (int y = 0; y < grid.length; y++) {
+            for (int x = 0; x < grid[0].length; x++) {
+                if (grid[y][x] == 10) {
+                    grid[y][x] = 0;
                         flashes++;
-                    } else {
-                        grid[y][x]++;
-                    }
+                } else {
+                    grid[y][x]++;
                 }
             }
         }
         return flashes;
     }
 
+    /**
+     * Go over each 9 and make it explode.
+     * If the resulted explosion has created a new 9, explode it as well.
+     */
+    private void explodeNines() {
+        while (hasNines(grid)) {
+            for (int y = 0; y < grid.length; y++) {
+                for (int x = 0; x < grid[0].length; x++) {
+                    if (grid[y][x] == 9) {
+                        explode(grid, y, x);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Find the nearby octopuses, and increase by 1. If the octopus is already at 9,
+     * ignore it as it will explode later.
+     * @param grid Grid of octopuses.
+     * @param y Y coordinate
+     * @param x X coordinate
+     */
     private void explode(int[][] grid, int y, int x) {
         for (int dy = -1; dy <= 1; dy++) {
             for (int dx = -1; dx <= 1; dx++) {
@@ -48,6 +72,12 @@ public class Day11 extends Day {
         grid[y][x] = 10;
     }
 
+    /**
+     * Check if the grid has any 9's to explode.
+     *
+     * @param grid grid to check.
+     * @return If any 9's are available.
+     */
     private boolean hasNines(int[][] grid) {
         for (int[] ints : grid) {
             for (int x = 0; x < grid[0].length; x++) {
@@ -64,29 +94,19 @@ public class Day11 extends Day {
         boolean allFlashed = false;
         long i;
         for(i = 0; !allFlashed; i++) {
-            while (hasNines(grid)) {
-                for (int y = 0; y < grid.length; y++) {
-                    for (int x = 0; x < grid[0].length; x++) {
-                        if (grid[y][x] == 9) {
-                            explode(grid, y, x);
-                        }
-                    }
-                }
-            }
+            explodeNines();
             allFlashed = hasAllFlashed(grid);
-            for (int y = 0; y < grid.length; y++) {
-                for (int x = 0; x < grid[0].length; x++) {
-                    if (grid[y][x] == 10) {
-                        grid[y][x] = 0;
-                    } else {
-                        grid[y][x]++;
-                    }
-                }
-            }
+            increaseValue();
         }
         return i;
     }
 
+    /**
+     * Checks if every item in the grid has flashed.
+     *
+     * @param grid Grid to check.
+     * @return If there are only 10's in the grid.
+     */
     private static boolean hasAllFlashed(int[][] grid) {
         for (int[] ints : grid) {
             for (int x = 0; x < grid[0].length; x++) {
