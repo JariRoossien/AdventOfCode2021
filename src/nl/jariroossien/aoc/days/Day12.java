9e2cfbb5217cc5926e7b31679966612b9654bcca
Jari Roossien <jariroossien@gmail.com>
2021-12-12 06:53:36 +0100

    Day 12 removed useless code.

--- src/nl/jariroossien/aoc/days/Day12.java
+++ src/nl/jariroossien/aoc/days/Day12.java
@@ -182,24 +182,4 @@ public class Day12 extends Day {
         }
     }
 
-    private static class Path {
-        Stack<Node> nodes;
-
-        public Path(Stack<Node> nodes) {
-            this.nodes = nodes;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Path path = (Path) o;
-            return Objects.equals(nodes, path.nodes);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(nodes);
-        }
-    }
 }
