817126bbefd345c5b829b41eee66e8d93229975b
Jari Roossien <jariroossien@gmail.com>
2021-12-14 19:01:04 +0100

    Completed Day 14

--- README.md
+++ README.md
@@ -25,51 +25,52 @@ public void setup() {
 
 ## Current Output:
 
-Run from slow laptop
-
 ```
 Day 1 challenge 1: 1266. solved in 0ms
 Day 1 challenge 2: 1217. solved in 0ms
 
-Day 2 challenge 1: 1762050. solved in 3ms
+Day 2 challenge 1: 1762050. solved in 2ms
 Day 2 challenge 2: 1855892637. solved in 1ms
 
 Day 3 challenge 1: 3847100. solved in 0ms
-Day 3 challenge 2: 4105235. solved in 2ms
+Day 3 challenge 2: 4105235. solved in 3ms
 
 Day 4 challenge 1: 63552. solved in 0ms
 Day 4 challenge 2: 9020. solved in 2ms
 
-Day 5 challenge 1: 5632. solved in 9ms
-Day 5 challenge 2: 22213. solved in 3ms
+Day 5 challenge 1: 5632. solved in 7ms
+Day 5 challenge 2: 22213. solved in 4ms
 
-Day 6 challenge 1: 389726. solved in 44ms
-Day 6 challenge 2: 1743335992042. solved in 4ms
+Day 6 challenge 1: 389726. solved in 42ms
+Day 6 challenge 2: 1743335992042. solved in 3ms
 
-Day 7 challenge 1: 336040. solved in 46ms
-Day 7 challenge 2: 94813675. solved in 375ms
+Day 7 challenge 1: 336040. solved in 47ms
+Day 7 challenge 2: 94813675. solved in 377ms
 
 Day 8 challenge 1: 543. solved in 0ms
 Day 8 challenge 2: 994266. solved in 0ms
 
 Day 9 challenge 1: 480. solved in 1ms
-Day 9 challenge 2: 1045660. solved in 11ms
+Day 9 challenge 2: 1045660. solved in 21ms
 
-Day 10 challenge 1: 390993. solved in 6ms
+Day 10 challenge 1: 390993. solved in 4ms
 Day 10 challenge 2: 2391385187. solved in 1ms
 
-Day 11 challenge 1: 1732. solved in 2ms
+Day 11 challenge 1: 1732. solved in 1ms
 Day 11 challenge 2: 290. solved in 1ms
 
-Day 12 challenge 1: 4573. solved in 9ms
-Day 12 challenge 2: 117509. solved in 355ms
+Day 12 challenge 1: 4573. solved in 12ms
+Day 12 challenge 2: 117509. solved in 444ms
 
-Day 13 challenge 1: 788. solved in 8ms
+Day 13 challenge 1: 788. solved in 7ms
 █  █   ██ ███  █  █ ████ █  █ ███   ██  
 █ █     █ █  █ █ █  █    █  █ █  █ █  █ 
 ██      █ ███  ██   ███  █  █ ███  █    
 █ █     █ █  █ █ █  █    █  █ █  █ █ ██ 
 █ █  █  █ █  █ █ █  █    █  █ █  █ █  █ 
 █  █  ██  ███  █  █ ████  ██  ███   ███ 
-Day 13 challenge 2: 0. solved in 9ms
+Day 13 challenge 2: 0. solved in 5ms
+
+Day 14 challenge 1: 2712. solved in 7ms
+Day 14 challenge 2: 8336623059567. solved in 8ms
 ```
---
+++ src/nl/jariroossien/aoc/days/Day14.java
@@ -0,0 +1,104 @@
+package nl.jariroossien.aoc.days;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Day14 extends Day {
+
+    HashMap<Pair, Long> pairCount = new HashMap<>();
+
+    @Override
+    public long solveOne() {
+        return calculateDifference(10);
+    }
+
+    @Override
+    public long solveTwo() {
+        pairCount = new HashMap<>();
+        setup();
+        return calculateDifference(40);
+    }
+
+    private long calculateDifference(int days) {
+        for (int i = 0; i < days; i++) {
+            HashMap<Pair, Long> newMap = new HashMap<>();
+            for (Pair p : pairCount.keySet()) {
+                Pair left = Pair.getNewPair(p.to[0]);
+                Pair right = Pair.getNewPair(p.to[1]);
+                newMap.merge(left, pairCount.get(p), Long::sum);
+                newMap.merge(right, pairCount.get(p), Long::sum);
+            }
+            pairCount = newMap;
+        }
+
+        Map<Character, Long> count = new HashMap<>();
+        pairCount.entrySet().stream().forEach(pair -> {
+            count.merge(pair.getKey().from.charAt(0), pair.getValue(), Long::sum);
+            count.merge(pair.getKey().from.charAt(1), pair.getValue(), Long::sum);
+        });
+        count.forEach((entry, value) -> {
+            count.put(entry, value / 2);
+            if (entry == 'S' || entry == 'V') {
+                count.merge(entry, 1L, Long::sum);
+            }
+        });
+        List<Map.Entry<Character, Long>> sorted = count.entrySet().stream().sorted((a, b) -> Long.compare(b.getValue(), a.getValue())).collect(Collectors.toList());
+        return sorted.get(0).getValue() - sorted.get(sorted.size() - 1).getValue();
+    }
+
+    @Override
+    public void setup() {
+        super.setup();
+        String init = input.get(0);
+
+        for (int i = 2; i < input.size(); i++) {
+            String[] cut = input.get(i).split(" -> ");
+            Pair temp = new Pair(cut[0], cut[1]);
+            Pair.addPair(temp);
+        }
+        for (int i = 0; i < init.length() - 1; i++) {
+            Pair temp = Pair.getNewPair(init.substring(i, i+2));
+            pairCount.merge(temp, 1L, Long::sum);
+        }
+    }
+
+    private static class Pair {
+        private final String from;
+        private String[] to;
+
+        private static final Set<Pair> pairSet = new HashSet<>();
+
+        public static void addPair(Pair p) {
+            pairSet.add(p);
+        }
+
+        public static Pair getNewPair(String pair) {
+            for (Pair p : pairSet) {
+                if (p.from.equalsIgnoreCase(pair)) {
+                    return p;
+                }
+            }
+            return null;
+        }
+
+        public Pair(String from, String to) {
+            this.from = from;
+            this.to = new String[2];
+            this.to[0] = from.charAt(0) + to;
+            this.to[1] = to + from.charAt(1);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Pair pair = (Pair) o;
+            return Objects.equals(from, pair.from);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(from);
+        }
+    }
+}
